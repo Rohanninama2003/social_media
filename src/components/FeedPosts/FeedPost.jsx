4a8b0393f2121ede5142c6784e9cdc9dae237971
Rohanninama2003 <104648723+Rohanninama2003@users.noreply.github.com>
2024-06-19 12:42:19 +0530

    Update FeedPost.jsx

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -8,7 +8,7 @@ const FeedPost = ({ post }) => {
 
 	return (
 			
-			<Box border={"3px solid"} borderColor={"whiteAlpha.800"} background={"#bac7c4"} borderRadius={"2xl"} padding={"3"} mb={5} color={"white"}>
+			<Box  padding={"3"} mb={5} color={"white"}>
 			<PostHeader post={post} creatorProfile={userProfile} />
 			<Box my={2} borderRadius={10} overflow={"hidden"}>
 				<Image src={post.imageURL} alt={"FEED POST IMG"} />
